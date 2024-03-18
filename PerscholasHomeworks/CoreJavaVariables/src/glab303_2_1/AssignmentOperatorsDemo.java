62e07a977c0373e54fea64ac1b5b4c4a70ee02ab
Hasib Johar <jeohar@outlook.com>
2024-03-18 07:31:08 -0700

    Assignment Operator Practice

---
+++ PerscholasHomeworks/CoreJavaVariables/src/glab303_2_1/AssignmentOperatorsDemo.java
@@ -0,0 +1,26 @@
+package glab303_2_1;
+
+public class AssignmentOperatorsDemo {
+
+
+    public static void main(String[] args) {
+
+        // Assigning primitive values
+        int j, k;
+        j = 10;
+        j = 5;
+        k = j;
+        System.out.println("J is : " + j);
+        System.out.println("k is : " + k);
+            // Multiplication Assignments
+        
+        k = j = 10; // (k = (j= 10))
+        System.out.println("j is :" + j);
+        System.out.println("k is :" + k);
+
+
+
+
+
+    }
+}
